1b60b261577308d6d4b45146aa96223e0cad9fc6
Woosung_Kim <sibu2005@naver.com>
2023-04-05 17:19:42 +0900

    feat : add member and address entity

---
+++ src/main/java/com/woosung/shop/domain/Address.java
@@ -0,0 +1,23 @@
+package com.woosung.shop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Builder
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
