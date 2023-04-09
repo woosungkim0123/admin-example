f678c6537551c74d4794cc3023167e64457c2a48
Woosung_Kim <sibu2005@naver.com>
2023-04-09 22:28:51 +0900

    feat : add findEmail for login

--- src/main/java/com/woosung/shop/domain/member/MemberRepository.java
+++ src/main/java/com/woosung/shop/domain/member/MemberRepository.java
@@ -3,4 +3,6 @@ package com.woosung.shop.domain.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
 }
