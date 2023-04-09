65695dfc80c4e36d4fde195b44ffa3743f001534
Woosung_Kim <sibu2005@naver.com>
2023-04-09 23:44:41 +0900

    feat : add findByEmail

--- src/main/java/com/woosung/shop/domain/member/MemberRepository.java
+++ src/main/java/com/woosung/shop/domain/member/MemberRepository.java
@@ -2,7 +2,9 @@ package com.woosung.shop.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
