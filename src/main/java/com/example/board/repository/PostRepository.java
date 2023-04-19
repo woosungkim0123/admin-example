2ba8702dccaa62a9290b0e5b9ec2885c86fa14f5
Woosung_Kim <sibu2005@naver.com>
2023-04-19 21:12:39 +0900

    chore : Spring Data Rest Option (#1)
    make post and postComment json api automatically

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.example.board.repository;
 
 import com.example.board.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
