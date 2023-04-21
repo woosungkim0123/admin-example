9bba34c3ca9c5c93b6fa63d370b13bf8fdc067b5
Woosung_Kim <sibu2005@naver.com>
2023-04-22 00:01:55 +0900

    feat : add querydsl api search option (#1)
    QuerydslPredicateExecutor, QuerydslBinderCustomizer

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,9 +1,33 @@
 package com.example.board.repository;
 
 import com.example.board.domain.Post;
+
+import com.example.board.domain.QPost;
+
+import com.querydsl.core.types.dsl.DateTimeExpression;
+
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends
+        JpaRepository<Post, Long>,
+        QuerydslPredicateExecutor<Post>,
+        QuerydslBinderCustomizer<QPost>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QPost root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
