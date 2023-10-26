088b18042e08ed56b80c5f8e0d0dabac2159ab3f
Woosung_Kim <sibu2005@naver.com>
2023-10-26 23:53:53 +0900

    feat : 메인 페이지 임시적으로 redirect 하도록 변경

---
+++ src/test/java/com/example/board/controller/MainControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class MainControllerTest {
+    private MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenReturnsRedirect() throws Exception {
+        // when
+        ResultActions result = mvc.perform(get("/"));
+
+        // then
+        result.andExpect(status().is3xxRedirection());
+    }
+
+}
